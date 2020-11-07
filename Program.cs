36567ea25c581a817bb324db9e1b414894f010b0
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-11-06 23:58:21 -0300

    Resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int idade;
+
+        Console.WriteLine("Avalie se você pode ser doador de sangue: ");
+        Console.Write("Qual a sua idade? ");
+        idade = Convert.ToInt32(Console.ReadLine());
+
+        if(idade <= 16)
+        {
+            Console.WriteLine("\nVocê não pode doar sangue");
+        }
+        if(idade >= 17 && idade <= 69)
+        {
+            Console.WriteLine("\nVocê pode doar sangue!");
+        }
+        if(idade >= 70)
+        {
+            Console.WriteLine("\nVocê não pode doar sangue");
+        }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # DoadorSangue
 Resposta ao exercício "DoadorSangue"
+
+**Enunciado**
+
+Solicite a idade do usuário. Avalie se ele pode ser doador de sangue (i.e. possui idade entre 18 e 67 anos). Informe o resultado da avaliação.
+
+Ex.:
+
+Qual a sua idade: 35
+Você pode ser doador de sangue.
+ou
+
+Qual a sua idade: 12
+Você não pode ser doador de sangue.
+ou
+
+Qual a sua idade: 80
+Você não pode ser doador de sangue.
+
+**Como executar:**
+
+Abra o programa e digite:
+
+dotnet run
